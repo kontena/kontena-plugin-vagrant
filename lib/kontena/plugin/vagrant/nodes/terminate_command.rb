018c803643fb3fb1efb14f82fcc19ad261a68bfe
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-08-04 13:55:43 +0300

    confirm node terminate

--- lib/kontena/plugin/vagrant/nodes/terminate_command.rb
+++ lib/kontena/plugin/vagrant/nodes/terminate_command.rb
@@ -4,13 +4,16 @@ module Kontena::Plugin::Vagrant::Nodes
     include Kontena::Cli::GridOptions
 
     parameter "NAME", "Node name"
+    option '--force', :flag, 'Force remove', default: false, attribute_name: :forced
 
     def execute
       require_api_url
       require_current_grid
 
+      confirm_command("#{current_grid}/#{name}") unless forced?
+
       require_relative '../../../machine/vagrant'
-      
+
       destroyer = destroyer(client(require_token))
       destroyer.run!(current_grid, name)
     end
