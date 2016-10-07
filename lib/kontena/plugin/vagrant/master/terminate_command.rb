61e881273cbc4c4ff21e605c57df7f5e9f20d558
Lauri Nevala <lauri.nevala@gmail.com>
2016-10-07 10:35:07 +0300

    Do not require access token when terminating master

--- lib/kontena/plugin/vagrant/master/terminate_command.rb
+++ lib/kontena/plugin/vagrant/master/terminate_command.rb
@@ -5,17 +5,9 @@ module Kontena::Plugin::Vagrant::Master
     option '--force', :flag, 'Force remove', default: false, attribute_name: :forced
 
     def execute
-      require_api_url
-
       confirm unless forced?
-
       require_relative '../../../machine/vagrant'
-      destroyer = destroyer(client(require_token))
-      destroyer.run!
-    end
-
-    def destroyer(client)
-      Kontena::Machine::Vagrant::MasterDestroyer.new(client)
+      Kontena::Machine::Vagrant::MasterDestroyer.new.run!
     end
   end
 end
