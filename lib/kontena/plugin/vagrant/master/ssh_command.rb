eb8ea7e77bb1651905b23f686910514edd7e7b63
Kimmo Lehto <kimmo.lehto@gmail.com>
2017-01-18 15:43:18 +0200

    Vagrant ssh commands accept passing commands

--- lib/kontena/plugin/vagrant/master/ssh_command.rb
+++ lib/kontena/plugin/vagrant/master/ssh_command.rb
@@ -2,14 +2,22 @@ module Kontena::Plugin::Vagrant::Master
   class SshCommand < Kontena::Command
     include Kontena::Cli::Common
 
+    parameter "[COMMANDS] ...", "Run command on Master"
+
     def execute
+      require 'shellwords'
       require_relative '../../../machine/vagrant'
 
       vagrant_path = "#{Dir.home}/.kontena/vagrant_master"
       abort("Cannot find Vagrant kontena-master".colorize(:red)) unless Dir.exist?(vagrant_path)
 
+      cmd = "vagrant ssh"
+      if self.commands_list && !self.commands_list.empty?
+        cmd << " -c '#{self.commands_list.shelljoin}'"
+      end
+
       Dir.chdir(vagrant_path) do
-        system('vagrant ssh')
+        system(cmd)
       end
     end
   end
--- lib/kontena/plugin/vagrant/nodes/ssh_command.rb
+++ lib/kontena/plugin/vagrant/nodes/ssh_command.rb
@@ -4,18 +4,24 @@ module Kontena::Plugin::Vagrant::Nodes
     include Kontena::Cli::GridOptions
 
     parameter "NAME", "Node name"
+    parameter "[COMMANDS] ...", "Run command on node"
 
     def execute
       require_api_url
       require_current_grid
 
+      require 'shellwords'
       require_relative '../../../machine/vagrant'
 
       vagrant_path = "#{Dir.home}/.kontena/#{current_grid}/#{name}"
       abort("Cannot find Vagrant node #{name}".colorize(:red)) unless Dir.exist?(vagrant_path)
 
+      cmd = "vagrant ssh"
+      if self.commands_list && !self.commands_list.empty?
+        cmd << " -c '#{self.commands_list.shelljoin}'" 
+      end
       Dir.chdir(vagrant_path) do
-        system('vagrant ssh')
+        system(cmd)
       end
     end
   end
